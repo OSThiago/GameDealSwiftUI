ec9c3a1316700ddde5bcf74db7dc4d943733390a
Thiago <thiagos2000@gmail.com>
2023-06-15 10:26:46 -0300

    Initial Commit

---
+++ GameDealSwiftUI/GameDealSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameDealSwiftUIApp.swift
+//  GameDealSwiftUI
+//
+//  Created by Thiago de Oliveira Sousa on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GameDealSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

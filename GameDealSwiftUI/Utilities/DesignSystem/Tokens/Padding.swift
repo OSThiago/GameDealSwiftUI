39d513fb928f74fd65138fb96ec36e7c44d9e944
Thiago <52988009+OSThiago@users.noreply.github.com>
2024-07-24 12:46:30 -0300

    feat: create design tokens

---
+++ GameDealSwiftUI/Utilities/DesignSystem/Tokens/Padding.swift
@@ -0,0 +1,56 @@
+//
+//  Padding.swift
+//  GameDealSwiftUI
+//
+//  Created by Thiago de Oliveira Sousa on 24/07/24.
+//
+
+import Foundation
+
+class Padding {
+    /// 0
+    let none: Double
+    /// 4.0
+    let quarck: Double
+    /// 8.0
+    let nano: Double
+    /// 12.0
+    let micro: Double
+    /// 16.0
+    let xxxs: Double
+    /// 24.0
+    let xxs: Double
+    /// 32.0
+    let xs: Double
+    /// 40.0
+    let sm: Double
+    /// 48.0
+    let md: Double
+    /// 56.0
+    let lg: Double
+    /// 72.0
+    let xl: Double
+    /// 96.0
+    let xxl: Double
+    /// 120.0
+    let xxxl: Double
+    /// 160.0
+    let huge: Double
+    
+    init() {
+        self.none = 0
+        self.quarck = 4.0
+        self.nano = 8.0
+        self.micro = 12.0
+        self.xxxs = 16.0
+        self.xxs = 24.0
+        self.xs = 32.0
+        self.sm = 40.0
+        self.md = 48.0
+        self.lg = 56.0
+        self.xl = 72.0
+        self.xxl = 96.0
+        self.xxxl = 120.0
+        self.huge = 160.0
+    }
+}
---
+++ GameDealSwiftUI/Utilities/DesignSystem/Tokens/Tokens.swift
@@ -0,0 +1,12 @@
+//
+//  Token.swift
+//  GameDealSwiftUI
+//
+//  Created by Thiago de Oliveira Sousa on 24/07/24.
+//
+
+import Foundation
+
+enum Tokens {
+    static let padding = Padding()
+}

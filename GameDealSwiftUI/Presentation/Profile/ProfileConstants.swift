7524421dc360d0ce627aaff5594090b85031625a
Thiago <thiagos2000@gmail.com>
2024-12-24 10:51:03 -0300

    feat: add cover icon size to constants file

--- GameDealSwiftUI/Presentation/Profile/ProfileConstants.swift
+++ GameDealSwiftUI/Presentation/Profile/ProfileConstants.swift
@@ -23,6 +23,7 @@ struct ProfileConstants {
     let coverHeight: CGFloat = (ScreenSize.width - 24) * 9/18
     let coverRadius: CGFloat = 12
     let coverPlaceholderIcon = "gamecontroller.fill"
+    let coverIconSize = 70
     // User Info
     let userNamePlaceholder = "User Name"
     let gamesCountTitle = "Games"

6efba3ed99c64997d26815277aaf075bce19d8b8
Thiago <thiagos2000@gmail.com>
2025-02-04 23:14:48 -0300

    feat: add alerts service to app asslmbler

--- GameDealSwiftUI/Root/AppAssembler.swift
+++ GameDealSwiftUI/Root/AppAssembler.swift
@@ -42,5 +42,8 @@ struct AppAssembler: Assembler {
         
         DependencyInjectionContainer.register(type: RedirectProtocol.self,
                                               RedirectServiceImplementation())
+        
+        DependencyInjectionContainer.register(type: AlertsProtocol.self,
+                                              AlertsServiceImplementation())
     }
 }
